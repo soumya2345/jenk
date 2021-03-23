0db06f0bbb22f10247835afdbc17ab3f8bf668f4
soumya2345 <80831758+soumya2345@users.noreply.github.com>
2021-03-23 21:35:58 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 echo "This is an example of a script"
 echo "That is running in build step"
-echo "This is a param $PASSED"
-NUM=$(10 + 5)
-echo "$NUM"
-
+# Calculate the sum of two integers with pre initialize values
+# in a shell script
+ 
+a=10
+b=20
+ 
+sum=$(( $a + $b ))
+ 
+echo $sum
