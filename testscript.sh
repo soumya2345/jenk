e2be9d9fbf34fd590812b5c1575c82fa5af13c35
soumya2345 <80831758+soumya2345@users.noreply.github.com>
2021-03-23 22:07:05 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,9 +3,9 @@ echo "This is an example of a script"
 echo "That is running in build step"
 # Calculate the sum of two integers with pre initialize values
 # in a shell script
+a=10
+b=20
  
-read -p "Enter first number: " num1
-read -p "Enter second number: " num2
-sum=$(( $num1 + $num2 ))
+sum=$(( $a + $b ))
  
 echo $sum
